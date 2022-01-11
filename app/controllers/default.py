ab77687ed7dfc3c0ad693eaa13da1c1c4e6defa3
Jorge Fernando Martins da Costa <jorgecosta@museu-goeldi.br>
2022-01-11 17:11:22 -0300

    Codificação das rotas de ordenação e consulta

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -8,5 +8,42 @@ def listagem():
     pessoas = Pessoa.query.all()
     return render_template('listagem.html', pessoas=pessoas, ordem='id')
 
+@app.route('/selecao/<int:id>')
+def selecao(id=0):
+    pessoas = Pessoa.query.filter_by(id=id).all()
+    return render_template('listagem.html', pessoas=pessoas, ordem='id')
+
+@app.route('/ordenacao/<campo>/<ordem_anterior>')
+def ordenacao(campo='id', ordem_anterior=''):
+    if campo == 'id':
+        if ordem_anterior == campo:
+            pessoas = Pessoa.query.order_by(Pessoa.id.desc()).al()
+        else:
+            pessoas = Pessoa.query.order_by(Pessoa.id).all()
+    elif campo == 'nome':
+        if ordem_anterior == campo:
+            pessoas = Pessoa.query.order_by(Pessoa.nome.desc()).al()
+        else:
+            pessoas = Pessoa.query.order_by(Pessoa.nome).all()
+    elif campo == 'idade':
+        if ordem_anterior == campo:
+            pessoas = Pessoa.query.order_by(Pessoa.idade.desc()).al()
+        else:
+            pessoas = Pessoa.query.order_by(Pessoa.idade).all()
+    elif campo == 'sexo':
+        if ordem_anterior == campo:
+            pessoas = Pessoa.query.order_by(Pessoa.sexo.desc()).al()
+        else:
+            pessoas = Pessoa.query.order_by(Pessoa.sexo).all()
+    elif campo == 'salario':
+        if ordem_anterior == campo:
+            pessoas = Pessoa.query.order_by(Pessoa.salario.desc()).al()
+        else:
+            pessoas = Pessoa.query.order_by(Pessoa.salario).all()
+    else:
+        pessoas = Pessoa.query.order_by(Pessoa.id).all()
+    return render_template('listagem.html', pessoas=pessoas, ordem=campo)
+
+
 
 
